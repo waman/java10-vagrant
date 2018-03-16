f3b19be6035f627c91c07f0c5581607622ebfbd0
waman <waman10da@gmail.com>
2018-03-16 22:41:19 +0900

    Refactor install_java10.sh

--- install_java10.sh
+++ install_java10.sh
@@ -6,8 +6,8 @@ done
 
 if [[ $jdk == "/vagrant/archive/*jdk*_linux-x64_bin.tar.gz" ]]; then
   defaultjdk=openjdk-10+46_linux-x64_bin.tar.gz
-  wget -q -O /vagrant/archive/$defaultjdk https://download.java.net/java/jdk10/archive/46/GPL/$defaultjdk
-  jdk=/vagrant/archive/$defaultjdk
+  wget -q -O /vagrant/archive/${defaultjdk} https://download.java.net/java/jdk10/archive/46/GPL/$defaultjdk
+  jdk=/vagrant/archive/${defaultjdk}
 else
   :
 fi
@@ -17,8 +17,7 @@ cd /opt/java
 tar zxvf ${jdk}
 cd /home/vagrant
 
-export JAVA_HOME=/opt/java/jdk-10
-export PATH=$PATH:$JAVA_HOME/bin
+javaHome=/opt/java/jdk-10
 
-echo "export JAVA_HOME=$JAVA_HOME" >> .bash_profile
+echo "export JAVA_HOME=${javaHome}" >> .bash_profile
 echo 'export PATH=$PATH:$JAVA_HOME/bin' >> .bash_profile
